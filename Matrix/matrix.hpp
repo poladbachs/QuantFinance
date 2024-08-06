e73e90f00e96eed34acff3a28e7f64a5ecde166e
Polad <bakhip@usi.ch>
2024-08-06 20:50:51 +0200

    done matrix declaration

--- Matrix/matrix.hpp
+++ Matrix/matrix.hpp
@@ -27,6 +27,24 @@ public:
     QSMatrix<T>& operator*=(const QSMatrix<T>& rhs);
     QSMatrix<T> transpose();
 
+    // Matrix scalar operations
+    QSMatrix<T> operator+(const T& rhs);
+    QSMatrix<T> operator-(const T& rhs);
+    QSMatrix<T> operator*(const T& rhs);
+    QSMatrix<T> operator/(const T& rhs);
+
+    // Matrix/vector operations
+    std::vector<T> operator*(const std::vector<T>& rhs);
+    std::vector<T> diag_vec();
+
+    // Access the individual elements
+    T& operator()(const unsigned& row, const unsigned& col);
+    const T& operator()(const unsigned& row, const unsigned& col) const;
+
+    // Access the row and column sizes
+    unsigned get_rows() const;
+    unsigned get_cols() const;
+
 };
 
 #include "matrix.cpp"
