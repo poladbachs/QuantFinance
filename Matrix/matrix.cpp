b3d384f8ea202d8ac264f0eb17f1e969bad7a01c
Polad <bakhip@usi.ch>
2024-08-07 03:07:33 +0200

    implemented assignment operator and matrices addition methods

--- Matrix/matrix.cpp
+++ Matrix/matrix.cpp
@@ -26,4 +26,46 @@ QSMatrix<T>::QSMatrix(const QSMatrix<T>& rhs) {
 template<typename T>
 QSMatrix<T>::~QSMatrix() {}
 
+// Assignment operator
+template<typename T>
+QSMatrix<T>& QSMatrix<T>::operator=(const QSMatrix<T>& rhs) {
+    if (&rhs == this) 
+        return *this;
+
+    unsigned new_rows = rhs.get_rows();
+    unsigned new_cols = rhs.get_cols();
+
+    mat.resize(new_rows);
+    for (unsigned i = 0; i < mat.size(); i++) {
+        mat[i].resize(new_cols);
+    }
+
+    for (unsigned i = 0; i < new_rows; i++) {
+        for (unsigned j = 0; i < new_cols; j++) {
+            mat[i][j] = rhs(i,j);
+        }
+    }
+    rows = new_rows;
+    cols = new_cols;
+
+    return *this;
+
+}
+
+// Addition of two matrices
+template<typename T>
+QSMatrix<T> QSMatrix<T>::operator+(const QSMatrix<T>& rhs) {
+    QSMatrix result(rows, cols, 0.0);
+
+    for (unsigned i = 0; i < rows; i++) {
+        for (unsigned j = 0; j < cols; j++) {
+            result(i,j) = this->mat[i][j] + rhs(i,j);
+        }
+    }
+
+    return result;
+}
+
+
+
 #endif
