cfe0bea5d6970712e0e72cd023829438ad8e7d0b
Polad <bakhip@usi.ch>
2024-08-07 06:26:46 +0200

    done Eigen Dot Cross Products and Reduction

--- Matrix Library/example_program.cpp
+++ Matrix Library/example_program.cpp
@@ -60,22 +60,47 @@
 //     std::cout << "p^T =\n" << p << std::endl; 
 // }
 
+// int main() {
+//     // Define a 3x3 matrix and two 3−dimensional vectors
+//     Eigen::Matrix3d p;
+//     p << 1,2,3,
+//          4,5,6,
+//          7,8,9;
+//     Eigen::Vector3d r(10, 11, 12); 
+//     Eigen::Vector3d s(13, 14, 15);
+
+//     // Matrix/matrix multiplication
+//     std::cout << "p*p:\n" << p*p << std::endl;
+
+//     // Matrix/vector multiplication
+//     std::cout << "p*r:\n" << p*r << std::endl;
+//     std::cout << "r^T*p:\n" << r.transpose()*p << std::endl;
+
+//     // Vector/vector multiplication (inner product)
+//     std::cout << "r^T*s:\n" << r.transpose()*s << std::endl;
+// }
+
+// int main() {
+//     // Declare and initialise two 3D vectors
+//     Eigen::Vector3d r(10, 20, 30); 
+//     Eigen::Vector3d s(40, 50, 60);
+
+//     // Apply the ’dot’ and ’cross’ products
+//     std::cout << "r . s =\n" << r.dot(s) << std::endl;
+//     std::cout << "r x s =\n" << r.cross(s) << std::endl;
+// }
+
 int main() {
-    // Define a 3x3 matrix and two 3−dimensional vectors
     Eigen::Matrix3d p;
-    p << 1,2,3,
-         4,5,6,
-         7,8,9;
-    Eigen::Vector3d r(10, 11, 12); 
-    Eigen::Vector3d s(13, 14, 15);
+    p << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
 
-    // Matrix/matrix multiplication
-    std::cout << "p*p:\n" << p*p << std::endl;
-
-    // Matrix/vector multiplication
-    std::cout << "p*r:\n" << p*r << std::endl;
-    std::cout << "r^T*p:\n" << r.transpose()*p << std::endl;
-
-    // Vector/vector multiplication (inner product)
-    std::cout << "r^T*s:\n" << r.transpose()*s << std::endl;
+    // Output the reduction operations
+    std::cout << "p.sum():" << p.sum() << std::endl;
+    std::cout << "p.prod():" << p.prod() << std::endl;
+    std::cout << "p.mean():" << p.mean() << std::endl;
+    std::cout << "p.minCoeff():" << p.minCoeff() << std::endl;
+    std::cout << "p.maxCoeff():" << p.maxCoeff() << std::endl;
+    std::cout << "p.trace():" << p.trace() << std::endl;
 }
