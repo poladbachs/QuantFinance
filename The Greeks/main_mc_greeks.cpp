9144b5721e22750b3c8588a181dcbb71dd09bd38
Polad <bakhip@usi.ch>
2024-08-10 21:53:27 +0200

    implemented calculation of greeks with fdm/mc method instead of analytical formulae

--- The Greeks/main_mc_greeks.cpp
+++ The Greeks/main_mc_greeks.cpp
@@ -44,3 +44,62 @@ void monte_carlo_call_price (const int num_sims, const double S,
     price_S = (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
     price_Sm = (payoff_sum_m / static_cast<double>(num_sims)) * exp(-r*T);
 }
+
+double call_delta_mc(const int num_sims, const double S, 
+                        const double K, const double r, 
+                        const double v, const double T, const double delta_S) {
+
+    // These values will be populated via the monte carlo call price function
+    // They represent the incremented Sp (S+delta S ) , non−incremented S (S) and
+    // decremented Sm (S−delta_S ) prices.
+    double price_Sp = 0.0;
+    double price_S = 0.0;
+    double price_Sm = 0.0;
+
+    // Call the Monte Carlo pricer for each of the three stock paths 
+    // (We only need two for the Delta)
+    monte_carlo_call_price(num_sims, S, K, r, v, T, delta_S , 
+                            price_Sp , price_S, price_Sm);
+    return (price_Sp - price_S)/delta_S;
+}
+
+double call_gamma_mc(const int num_sims, const double S, 
+                        const double K, const double r, 
+                        const double v, const double T, const double delta_S) {
+    double price_Sp = 0.0;
+    double price_S = 0.0;
+    double price_Sm = 0.0;
+    // Call the Monte Carlo pricer for each of the three stock paths 
+    // (We need all three for the Gamma)
+    monte_carlo_call_price(num_sims, S, K, r, v, T, delta_S , 
+                            price_Sp , price_S, price_Sm);
+    return (price_Sp - 2*price_S + price_Sm)/(delta_S*delta_S);
+}
+
+int main(int argc, char **argv) {
+    // First we create the parameter list
+    double S = 100.0; // Option price
+    double delta_S = 0.001; // Option price increment
+    double K = 100.0; // Strike price
+    double r = 0.05; // Risk−free rate (5%)
+    double v = 0.2; // Volatility of the underlying (20%) 
+    double T = 1.0; // One year until expiry
+    int num_sims = 100000000;// Number of simulations to carry out for monte carlo
+
+    double call_delta_m = call_delta_mc(num_sims, S, K, r, v, T, delta_S); 
+    double call_gamma_m = call_gamma_mc(num_sims, S, K, r, v, T, delta_S);
+
+    // Finally we output the parameters and prices
+    std::cout << "Number of Sims: " << num_sims << std::endl;
+    std::cout << "Underlying: " << S << std::endl;
+    std::cout << "Delta Underlying: " << delta_S << std::endl;
+    std::cout << "Strike: " << K << std::endl;
+    std::cout << "Risk-Free Rate: " << r << std::endl;
+    std::cout << "Volatility: " << v << std::endl;
+    std::cout << "Maturity: " << T << std::endl << std::endl;
+
+    std::cout << "Call Delta: " << call_delta_m << std::endl;
+    std::cout << "Call Gamma: " << call_gamma_m << std::endl;
+
+    return 0;
+}
