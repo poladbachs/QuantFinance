b60464a62334ca003129d4f1a77cead074a30492
Polad <bakhip@usi.ch>
2024-08-10 16:40:52 +0200

    monte carlo added and main greeks function implemented

--- The Greeks/black_scholes.h
+++ The Greeks/black_scholes.h
@@ -114,3 +114,29 @@ double put_rho(const double S, const double K, const double r ,
                   const double v , const double T) {
     return -T*K*exp(-r*T)*norm_cdf(-d_j(2, S, K, r, v, T));
 }
+
+
+// =========== 
+// MONTE CARLO 
+// ===========
+
+// A simple implementation of the Box−Muller algorithm , used to generate
+// gaussian random numbers − necessary for the Monte Carlo method below 
+// Note that C++11 actually provides std :: normal distribution<> in
+// the <random> library , which can be used instead of this function
+double gaussian_box_muller () {
+    double x = 0.0;
+    double y = 0.0;
+    double euclid_sq = 0.0;
+
+    // Continue generating two uniform random variables 
+    // until the square of their ”euclidean distance” 
+    // is less than unity
+    do {
+        x = 2.0 * rand() / static_cast<double>(RAND_MAX)-1; 
+        y = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
+        euclid_sq = x*x + y*y;
+    } while (euclid_sq >= 1.0);
+
+    return x*sqrt(-2*log(euclid_sq)/euclid_sq);
+}
