b8f2cc3dd57b5847b06435c542f4e521bc79c653
Polad <bakhip@usi.ch>
2024-08-10 16:06:30 +0200

    started calculating greeks using analytic formulae for call and put from euro options pricing

--- The Greeks/black_scholes.h
+++ The Greeks/black_scholes.h
@@ -2,6 +2,11 @@
 
 #include <iostream> 
 #include <cmath>
+#include <algorithm> // Needed for the ”max” function
+
+// ================= 
+// ANALYTIC FORMULAE 
+// =================
 
 // Standard normal probability density function
 double norm_pdf(const double& x) {
@@ -39,36 +44,3 @@ double call_price (const double& S, const double& K, const double& r,
                norm_cdf(d_j(2, S, K, r, v, T));
 }
 
-// Calculate the European vanilla put price based on
-// underlying S, strike K, risk−free rate r, volatility of
-// underlying sigma and time to maturity T
-double put_price (const double& S, const double& K, const double& r,
-                   const double& v, const double& T) {
-    return -S * norm_cdf(-d_j(1, S, K, r, v, T))+K*exp(-r*T) *
-               norm_cdf(-d_j(2, S, K, r, v, T));
-}
-
-int main(int argc, char **argv) {
-    // First we create the parameter list
-    double S = 100.0; // Option price
-    double K = 100.0; // Strike price
-    double r = 0.05; // Risk−free rate (5%)
-    double v = 0.2; // Volatility of the underlying (20%) 
-    double T = 1.0; // One year until expiry
-
-    // Then we calculate the call/put price
-    double call = call_price(S, K, r, v, T);
-    double put = put_price(S, K, r, v, T);
-
-    // Finally we output the parameters and prices
-    std::cout << "Underlying: " << S << std::endl;
-    std::cout << "Strike: " << K << std::endl;
-    std::cout << "Risk-Free Rate: " << r << std::endl;
-    std::cout << "Volatility: " << v << std::endl;
-    std::cout << "Maturity: " << T << std::endl;
-
-    std::cout << "Call Price: " << call << std::endl;
-    std::cout << "Put Price: " << put << std::endl;
-
-    return 0;
-}
