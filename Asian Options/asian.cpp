9000389d24a782282434ab8271b11d1e751808fc
Polad <bakhip@usi.ch>
2024-08-11 06:08:29 +0200

    asian option geometric implemented

--- Asian Options/asian.cpp
+++ Asian Options/asian.cpp
@@ -30,5 +30,25 @@ double AsianOptionArithmetic::pay_off_price(
     return (* pay_off ) ( arith_mean );
 }
 
+// ==================== 
+// AsianOptionGeometric 
+// ====================
+
+AsianOptionGeometric::AsianOptionGeometric(PayOff* pay_off )
+    : AsianOption(pay_off) {}
+
+// Geometric mean pay−off price
+double AsianOptionGeometric::pay_off_price(
+    const std::vector<double>& spot_prices) const {
+    
+    unsigned num_times = spot_prices.size();
+    double log_sum = 0.0;
+    for (int i=0; i<spot_prices.size(); i++) {
+        log_sum += log(spot_prices[i]);
+    }
+
+    double geom_mean = exp(log_sum / static_cast<double>(num_times));
+    return (*pay_off)(geom_mean);
+}
 
 #endif
