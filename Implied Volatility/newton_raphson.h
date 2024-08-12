fe3e29fa8ce99caf74c5720c604d7f3f1d7cda3e
Polad <bakhip@usi.ch>
2024-08-12 03:11:39 +0200

    done newton raphson

--- Implied Volatility/newton_raphson.h
+++ Implied Volatility/newton_raphson.h
@@ -14,6 +14,17 @@ double newton_raphson(double y_target , // Target y value
     // Set the initial option prices and volatility
     double y = (root_func.*g)(init); // Initial option prices 
     double x = init ; // Initial volatility
+
+    // While y and y target are not similar enough 
+    // Take the vega of the option and recalculate 
+    // a new call price based on the best linear 
+    // approximation at that particular vol value
+    while ( fabs (y - y_target ) > epsilon ) {
+        double d x = (root func.*g prime)(x); 
+        x += (y_target-y)/d x;
+        y = (root_func.*g)(x);
+    }
+    return x;
 }
 
 #endif
