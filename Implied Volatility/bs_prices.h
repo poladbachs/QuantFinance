a40581f983644d3a407013f1a2094419caf1773d
Polad <bakhip@usi.ch>
2024-08-12 02:54:03 +0200

    implementing newton-raphson, added option call vega formula

--- Implied Volatility/bs_prices.h
+++ Implied Volatility/bs_prices.h
@@ -42,4 +42,12 @@ double call_price (const double& S, const double& K, const double& r,
                norm_cdf(d_j(2, S, K, r, v, T));
 }
 
+// Calculate the European vanilla call vega ’Greek’ based on
+// underlying S, strike K, risk−free rate r, volatility of
+// underlying sigma and time to maturity T
+double call_vega(const double S, const double K, const double r ,
+                 const double sigma , const double T) {
+    return S * sqrt(T) * norm_pdf(d_j(1, S, K, r, sigma, T));
+}
+
 #endif
