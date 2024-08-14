b3360d76c5a787ec16efd94b7124f9c3b2f06548
Polad <bakhip@usi.ch>
2024-08-14 21:03:52 +0200

    FDM Euler Explicit class inherited from FDM Base

--- Finite Difference Methods/fdm.h
+++ Finite Difference Methods/fdm.h
@@ -31,9 +31,9 @@ protected:
     std::vector<double> old_result; // Old solution (becomes N)
 
     // Constructor
-    FDMBase(double _x_dom, unsigned long J,
-            double _t_dom, unsigned long N,
-            ConvectionDiffusionPDE* pde);
+    FDMBase(double _x_dom, unsigned long _J,
+            double _t_dom, unsigned long _N,
+            ConvectionDiffusionPDE* _pde);
     
     // Override these virtual methods in derived classes for
     // specific FDM techniques, such as explicit Euler, Crank−Nicolson, etc . 
@@ -41,6 +41,26 @@ protected:
     virtual void set_initial_conditions () = 0;
     virtual void calculate_boundary_conditions() = 0;
     virtual void calculate_inner_domain() = 0;
+
+public:
+    // Carry out the actual time−stepping 
+    virtual void step_march () = 0;
 };
+
+class FDMEulerExplicit : public FDMBase {
+protected:
+    void calculate_step_sizes();
+    void set_initial_conditions();
+    void calculate_boundary_conditions(); 
+    void calculate_inner_domain();
+
+public:
+    FDMEulerExplicit(double _x_dom, unsigned long _J,
+                     double _t_dom, unsigned long _N,
+                     ConvectionDiffusionPDE* _pde);
+
+    void step_march();
+};
+
 
 #endif
