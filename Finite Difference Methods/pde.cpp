c370d70507b262f82a60fdaeca9a9bbdd7362a60
Polad <bakhip@usi.ch>
2024-08-14 20:36:52 +0200

    added boundary and init conditions in pde

--- Finite Difference Methods/pde.cpp
+++ Finite Difference Methods/pde.cpp
@@ -23,4 +23,20 @@ double BlackScholesPDE::source_coeff(double t, double x) const {
     return 0.0;
 }
 
+// Left boundary condition (vanilla call option)
+double BlackScholesPDE::boundary_left(double t, double x) const {
+    return 0.0; // Specifically for a CALL option
+}
+
+// Right boundary condition (vanilla call option)
+double BlackScholesPDE::boundary_right(double t, double x) const {
+    // This is via Put-Call Parity and works for a call option
+    return (x - (option->K) * exp(-(option->r) * ((option->T) - t)));
+}
+
+// Initial condition (vanilla call option)
+double BlackScholesPDE::init_cond(double x) const {
+    return option->pay_off->operator()(x);
+}
+
 #endif
