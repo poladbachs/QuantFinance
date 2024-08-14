3e662721e68e01607722cec1de5a7fc03a203fa8
Polad <bakhip@usi.ch>
2024-08-14 19:25:30 +0200

    done with payoff for fdm solver

--- Finite Difference Methods/payoff.cpp
+++ Finite Difference Methods/payoff.cpp
@@ -17,5 +17,16 @@ double PayOffCall::operator()(const double& S) const {
     return std::max(S - K, 0.0); // Standard European call pay-off
 }
 
+// ========= 
+// PayOffPut 
+// =========
+
+// Constructor with single strike parameter
+PayOffPut::PayOffPut(const double& _K) { K = _K; }
+
+// Overridden operator() method, which turns PayOffCall into a function object
+double PayOffPut::operator() (const double& S) const {
+    return std::max(K - S, 0.0); // Standard European put pay-off
+}
 
 #endif
